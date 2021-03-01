0b306d7efdaf37cf1ca156b4c3cd43fe750bda3d
Adam Page <adampage1993@gmail.com>
2021-03-01 17:25:11 +0000

    initialise game board and snake

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 // Colors
-const boardBorderColor = '';
-const boardColor = '#333';
-const snakeBorderColor = '';
+const boardColor = '#222';
 const snakeColor = '#F2F2F2';
 
-ctx.fillStyle = boardColor;
-ctx.fillRect(10, 10, canvas.width, canvas.height);
+let snake = [
+  { x: 200, y: 200 },
+  { x: 190, y: 200 },
+  { x: 180, y: 200 },
+  { x: 170, y: 200 },
+  { x: 160, y: 200 },
+];
+
+// GAME START
+function main() {
+  clearCanvas();
+  drawSnake();
+}
+
+main();
+
+function clearCanvas() {
+  ctx.fillStyle = boardColor;
+  ctx.fillRect(10, 10, canvas.width, canvas.height);
+}
+
+function drawSnake() {
+  snake.forEach(snakeSegment => {
+    console.log(snakeSegment);
+    ctx.fillStyle = snakeColor;
+    ctx.fillRect(snakeSegment.x, snakeSegment.y, 10, 10);
+  });
+}
